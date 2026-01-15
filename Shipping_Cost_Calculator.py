c4725f9dde40a1ff3307b2bc0c6e2ed3e8b93f7b
Marcus Yee <yeemarcus02@gmail.com>
2026-01-14 20:34:00 -0500

    Revert "added another comment"
    This reverts commit 447dd8d978db4d8d36414de83b493772cd671a33.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,6 +1,5 @@
 # Shipping Cost Calculator
 # Here is a new update by MarcusYee3
-# Here is a another update by MarcusYee3
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
